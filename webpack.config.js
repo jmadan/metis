0a24a14a1ab0ff4c105b279d944786ba4ba12092
Jasdeep Madan <jasdeepm@gmail.com>
2017-04-21 14:54:33 +1000

    adding package.json, webpack config file and Home component

--- webpack.config.js
+++ webpack.config.js
@@ -4,18 +4,23 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './app/index.js',
   output: {
-    path: PATH.resolve(__dirname, 'build'),
+    path: PATH.resolve(__dirname, '/build'),
     filename: 'app_bundle.js'
   },
   module: {
     rules: [
       { test: /\.(js)$/, use: 'babel-loader' },
-      { test: /\.css$/, use: [ 'style-loader', 'css-loader']}
+      { test: /\.css$/, use: [ 'style-loader', 'css-loader']},
+      {test: /\.(png|jpg|gif)$/, loader: "url?limit=25000"},
+			{test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader'},
+      {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
     ]
   },
   plugins: [
     new HTMLWebpackPlugin({
-      template: 'app/views/layout.html'
+      template: './app/views/layout.html'
     })
   ]
 };
