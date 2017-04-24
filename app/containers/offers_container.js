3364ffbc0a4fc0520e62eabb58acb5eb2b5aaa65
Jasdeep Madan <jasdeepm@gmail.com>
2017-04-24 13:40:48 +1000

    offers container to maintain the state and load the offers - currently from a json file - in future it should be replaced with API call to load from backend system

--- app/containers/offers_container.js
+++ app/containers/offers_container.js
@@ -1,24 +1,49 @@
 import React, { Component } from 'react';
 
 import OffersNav from '../components/offers/offers_nav';
+import OffersComponent from '../components/offers/offers_component';
+
+import * as Api from '../utils/api';
+import offersList from '../data/offers';
 
 class OffersContainer extends Component {
   constructor(props){
-    super(props);
+    super();
 
     this.state = {
-      offers: null
+      selectedFilter: 'SHOW_ALL',
+      offers: [],
     }
 
+    this.updateOffers = this.updateOffers.bind(this);
+  }
+
+  componentDidMount(){
+    this.updateOffers(this.state.selectedFilter);
+  }
+
+  updateOffers(filter){
+    console.log(filter);
+    this.setState(function(){
+      return {
+        selectedFilter: filter,
+        offers: offersList.content
+      };
+    });
+  }
+
+  shouldComponentUpdate(){
+    return true;
   }
 
   render(){
     return (
       <div className="row">
-        <OffersNav />
-
+        <OffersNav onClick={this.updateOffers}/>
+        <br/>
+        {!this.state.offers ? <p>Need to get some Offers</p> : <OffersComponent {...this.state} />}
       </div>
-    );
+    )
   }
 }
 
