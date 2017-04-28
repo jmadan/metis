e3964997bf39ee69f53b5a9e03126418a18a342a
Jasdeep Madan <jasdeepm@gmail.com>
2017-04-28 22:47:45 +1000

    included jquery and bootstrap.min.js files

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,24 @@
 const PATH = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+let pathsToClean = ['build/']
+
+// the clean options to use
+let cleanOptions = {
+  root:     '/',
+  verbose:  true,
+  dry:      false
+}
 
 module.exports = {
   entry: './app/index.js',
   devtool: 'cheap-module-source-map',
   output: {
-    path: PATH.resolve(__dirname, '/build'),
-    filename: 'app_bundle.js'
+    path: PATH.resolve(__dirname, 'build'),
+    filename: 'app_bundle.js',
+    publicPath: '/'
   },
   module: {
     rules: [
@@ -20,7 +31,11 @@ module.exports = {
       {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
     ]
   },
+  devServer: {
+    historyApiFallback: true
+  },
   plugins: [
+    new CleanWebpackPlugin(pathsToClean, cleanOptions),
     new HTMLWebpackPlugin({
       template: './app/views/layout.html'
     }),
