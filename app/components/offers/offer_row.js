5d6c8070ef24e297356274294fc55804aa831481
Jasdeep Madan <jasdeepm@gmail.com>
2017-05-01 00:41:01 +1000

    using mongo _id for unique key and offerId for identifying offer to edit

--- app/components/offers/offer_row.js
+++ app/components/offers/offer_row.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import moment from 'moment';
 
 function formatDate(d){
-  return moment.unix(d).format("L");
+  return moment.unix(d).format('LL');
 }
 
 const OfferRow = (props) => {
@@ -12,13 +12,14 @@ const OfferRow = (props) => {
   return (
     <tbody>
       {props.items.map((offer) => {
-        return (<tr key={offer.offerId}>
-    			<td><img src={`https://s3-ap-southeast-1.amazonaws.com/images/outlets/offers/${offer.offerId}.png`} alt="..." className="img-thumbnail" /></td>
+
+        return (<tr key={offer._id.$oid}>
+    			<td><img src={`https://s3-ap-southeast-1.amazonaws.com/images/outlets/offers/${offer._id.$oid}.png`} alt="..." className="img-thumbnail" /></td>
     			<td>{offer.title}</td>
     			<td>{offer.shortDescription}</td>
     			<td>{formatDate(offer.endsOn)}</td>
     			<td>
-    				<p><Link to="#" onClick={props.editOffer.bind(null, offer)} className="btn btn-success btn-default active">Edit</Link></p>
+    				<p><Link to={`/offers/offer/edit/${offer.offerId}`} className="btn btn-success btn-default active">Edit</Link></p>
     				<p><button className="btn btn-danger btn-default">Delete</button></p>
     			</td>
     		</tr>);
