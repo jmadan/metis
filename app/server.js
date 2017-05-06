3c13a162e421359337a5103a85e14e21c8016ee0
Jasdeep Madan <jasdeepm@gmail.com>
2017-05-06 22:06:01 +1000

    implemented specific '/' route to send index.html from build directory

--- app/server.js
+++ app/server.js
@@ -3,8 +3,12 @@ const express = require('express');
 
 var app = express();
 
-app.use(express.static(path.join(__dirname, '../build')));
+app.use(express.static(path.join(__dirname, 'build')));
 app.set('port', process.env.PORT || 8080);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/build/index.html');
+});
 
 var server = app.listen(app.get('port'), function() {
   console.log('listening on port ', server.address().port);
