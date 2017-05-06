524a4d0c3acf9f6035ed51b65d4006dac6526f2c
Jasdeep Madan <jasdeepm@gmail.com>
2017-05-06 10:04:16 +1000

    adding bial logo to the header

--- app/components/header/header_logo.js
+++ app/components/header/header_logo.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 const HeaderLayout = () => {
   return (
       <div className="col-md-3 no-padding">
-        <NavLink to="/">Admin Site</NavLink>
+        <NavLink to="/"><img src="imgs/kempegowdaLogo.png" alt="BIAL" height="100px" /></NavLink>
       </div>
   );
 }
